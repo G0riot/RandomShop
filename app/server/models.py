610a3092d65ef537f173e8094b2aba47d2ab2a17
G0riot <semeniuk.yurko@gmail.com>
2023-10-12 16:07:27 +0300

    add UpdateProduct class

--- app/server/models.py
+++ app/server/models.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class ProductSchema(BaseModel):
+    product_id: str = Field(...)
     name: str = Field(...)
     color: str = Field(...)
     weight: float = Field(gt=0)
@@ -10,9 +12,41 @@ class ProductSchema(BaseModel):
     class Config:
         schema_example = {
             "example": {
+                "product_id": "Random id",
                 "name": "Random product",
                 "color": "Random color",
                 "weight": "Random weight",
                 "brand": "Random brand"
             }
         }
+
+
+class UpdateProduct(BaseModel):
+    product_id: Optional[str]
+    name: str = Optional[str]
+    color: str = Optional[str]
+    weight: float = Optional[float]
+    brand: str = Optional[str]
+
+    class Config:
+        schema_example = {
+            "example": {
+                "product_id": "77777777",
+                "name": "Some product",
+                "color": "Green",
+                "weight": "777",
+                "brand": "FunnyLucky"
+            }
+        }
+
+
+def response_model(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+
+def error_response_model(error, code, message):
+    return {"error": error, "code": code, "message": message}
